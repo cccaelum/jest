b9313d7cea570a4dfdfcb027112ce050335ee1b2
cccaelum <ccc.trad@gmail.com>
2024-09-19 18:10:05 +0200

    '1stcommit'

---
+++ product.js
@@ -0,0 +1,10 @@
+let products = []; 
+let id = 0;
+
+module.exports = {
+    resetProducts,  
+    addProduct, 
+    removeProduct, 
+    getProduct, 
+    updateProduct
+  };
---
+++ product.test.js
@@ -0,0 +1,22 @@
+const { resetProducts,  addProduct, removeProduct, getProduct, updateProduct} = require('./product');
+
+
+describe('addProduct', () => {
+    it('should add a product', () => {})
+})
+
+describe('removeProduct', () => {
+    it('should remove a product', () => {})
+})
+
+describe('getProduct', () => {
+    it('should get a product', () => {})
+})
+
+describe('updateProduct', () => {
+    
+})
+
+describe('resetProducts', () => {
+
+})
